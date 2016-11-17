7051cbd5ef531ce1e7c0122edc93796a2b855359
John Doe <blank>
2016-11-17 11:05:54 -0200

    initial commit

---
+++ rm.c
@@ -0,0 +1,284 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdbool.h>
+#include <unistd.h>
+#include <dirent.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <errno.h>
+#include <string.h>
+
+void printUsage() {
+	fprintf(stderr, "This program removes files or directories\n"
+			"Usage: rm [-rdiv] [<file/directory> ...]\n"
+			"\t-r: recursive\n"
+			"\t-d: empty directories\n"
+			"\t-i: interactive\n"
+			"\t-v: verbose\n"
+			"For more information, please consult man\n");
+}
+
+bool isDirectory(char *path) {
+	struct stat statBuffer;
+
+	if(lstat(path, &statBuffer) != -1) {
+		if(S_ISDIR(statBuffer.st_mode))
+			return true;
+		else
+			return false;
+	} else
+		printf("ERROR %d: %s\n", errno, strerror(errno));
+}
+
+bool isEmpty(char *path) {
+	int numberOfFiles = 0;
+
+	DIR *dir;
+	struct dirent *entry;
+
+	if((dir = opendir(path)) == NULL) {
+		printf("ERROR %d: %s\n", errno, strerror(errno));
+		return -1;
+	}
+
+	while((entry = readdir(dir)) != NULL) {
+		if(++numberOfFiles > 2)
+			break;
+	}
+
+	closedir(dir);
+	if(numberOfFiles <=2) //Empty
+		return true;
+	else
+		return false;
+}
+
+char* getFileName(char *path) {
+	char *nameStart, *fileName;
+	int fileNameLength;
+
+	nameStart = strrchr(path, '/');
+
+	if(nameStart != NULL) {
+		return nameStart+1;
+	}
+}
+
+void clearInputBuffer() {
+	char c;
+
+	while((c = getchar()) != '\n' && c != EOF) {}
+}
+
+bool invalidInput(char input) {
+	if(input != 'n' && input != 'N' && input != 'y' && input != 'Y')
+		return true;
+	else
+		return false;
+}
+
+bool positiveInput(char input) {
+	if(input == 'y' || input == 'Y') {
+		clearInputBuffer();
+		return true;
+	} else
+		return false;
+}
+
+bool negativeInput(char input) {
+		if(input == 'n' || input == 'N') {
+		clearInputBuffer();
+		return true;
+	} else
+		return false;
+}
+
+void walkPath(char *path, char *pathHistory, bool isInteractive, bool verbose) {
+	DIR *dir;
+	struct dirent *entry;
+	struct stat statBuffer;
+	char answer;
+	char *auxPath;
+	char tmp[1024];
+	int length;
+
+	if((dir = opendir(path)) == NULL) {
+		printf("ERROR %d: %s\n", errno, strerror(errno));
+		return;
+	}
+
+	auxPath = pathHistory + strlen(pathHistory);
+
+	chdir(path);
+
+	while((entry = readdir(dir)) != NULL) {
+		lstat(entry->d_name, &statBuffer);
+
+		if(strcmp(".", entry->d_name) == 0 || strcmp("..", entry->d_name) == 0)
+			continue;
+
+		strcpy(auxPath, entry->d_name);
+		
+		if(S_ISDIR(statBuffer.st_mode)) {
+			if(isInteractive) {
+				printf("Do you want to descend to directory '%s'? (y/n) - ", pathHistory);
+				answer = getc(stdin);
+				//Checking user input
+				if(invalidInput(answer))
+					return;
+				//If negative, skip to the next file/directory
+				if(negativeInput(answer))
+					continue;
+				//If positive, check files inside
+				if(positiveInput(answer)) {
+					strncpy(tmp, pathHistory, sizeof(tmp));
+					strcat(pathHistory, "/");
+
+					walkPath(entry->d_name, pathHistory, isInteractive, verbose);
+					
+					printf("Do you want to remove the directory '%s'? (y/n) - ", tmp);
+					answer = getc(stdin);
+					if(invalidInput(answer))
+						return;
+					if(negativeInput(answer))
+						continue;
+					if(positiveInput(answer) && verbose)
+						printf("You removed the directory '%s'.\n", tmp);
+				}
+			} else
+				if(verbose) {
+					strncpy(tmp, pathHistory, sizeof(tmp));
+					strcat(pathHistory, "/");
+					walkPath(entry->d_name, pathHistory, isInteractive, verbose);
+					printf("You removed the directory '%s'.\n", tmp);
+				}
+		} else
+			if(isInteractive) {
+				printf("Do you want to remove the file '%s'? (y/n) - ", pathHistory);
+				answer = getchar();
+				//Checking user input
+				if(invalidInput(answer))
+					return;
+				//If negative, skipsto the next file/directory
+				if(negativeInput(answer))
+					continue;
+				//If positive, remove the file
+				if(positiveInput(answer) && verbose)
+					printf("You removed the file '%s'.\n", pathHistory);
+			} else
+				if(verbose)
+					printf("You removed the file '%s'.\n", pathHistory);
+	}
+
+	chdir("..");
+	closedir(dir);
+}
+
+void removeFunction(char *path, bool isInteractive, bool verbose, bool ableToRemoveDirectory) {
+	if(isDirectory(path)) {
+		if(!ableToRemoveDirectory) {
+			printf("ERROR: You cannot remove a directory.\n");
+			return;
+		}
+		else 
+			if(!isEmpty(path)) {
+				printf("ERROR: The directory is not empty.\n");
+				return;
+		}
+	}
+
+	char *fileName = getFileName(path);
+
+	if(isInteractive) {
+		char answer;
+		printf("Do you want to remove the file '%s'? (y/n) - ", fileName);
+		answer = getc(stdin);
+		if(invalidInput(answer))
+			return;
+		else
+			if(positiveInput(answer) && verbose) {
+				printf("You removed the file '%s'.\n", fileName);
+			}
+	}
+
+	if(!isInteractive && verbose)
+		printf("You removed '%s'.\n", fileName);
+}
+
+void removeRecursivelyFunction(char *path, bool isInteractive, bool verbose) {
+	char *directoryName, *fullPath, *auxPath;
+	int length;
+
+	directoryName = getFileName(path);
+	length = strlen(directoryName);
+
+	//Allocating memory for the path
+	if((fullPath = malloc (length + NAME_MAX + 2)) == NULL) {
+		printf("ERROR %d: %s\n", errno, strerror(errno));
+		return;
+	}
+
+	memcpy(fullPath, directoryName, length);
+	auxPath = fullPath + length; *auxPath++ = '/';
+
+	walkPath(path, fullPath, isInteractive, verbose);
+
+	free(fullPath);
+}
+
+int main(int argc, char *argv[]) {
+	//removeRecursivelyFunction("/home/iroque/Documents/SO2/teste", true, true);
+	int option;
+	char path[1024];
+	getcwd(path, sizeof(path));
+	bool isInteractive, verbose, isRecursive, ableToRemoveDirectory;
+
+	if(argc == 1) {
+		printUsage();
+		return -1;
+	}
+
+	//argc needs to be = 3
+	if(argc > 3) {
+		printUsage();
+		return -1;
+	}
+
+	strcat(path, "/");
+
+	while((option = getopt(argc, argv, "rdiv")) != -1) {
+		switch(option) {
+			case 'r':
+				isRecursive = true;
+				break;
+			case 'd':
+				ableToRemoveDirectory = true;
+				break;
+			case 'i':
+				isInteractive = true;
+				break;
+			case 'v':
+				verbose = true;
+				break;
+			default:
+				printUsage();
+				break;
+		}
+	}
+
+	if(optind == 1 && argc == 2) { //No option
+		strcat(path, argv[1]);
+		removeFunction(path, isInteractive, verbose, ableToRemoveDirectory);
+	} else {
+		if(argc == 3) {
+			strcat(path, argv[2]);
+
+			if(isRecursive)
+				removeRecursivelyFunction(path, isInteractive, verbose);
+			else
+				removeFunction(path, isInteractive, verbose, ableToRemoveDirectory);
+		}
+	}
+
+	return 0;
+}
---
+++ touch.c
@@ -0,0 +1,172 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdbool.h>
+#include <unistd.h>
+#include <dirent.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <errno.h>
+#include <string.h>
+#include <limits.h>
+#include <time.h>
+#define DEBUG 1
+#define VERBOSE 1
+
+void printUsage() {
+	fprintf(stderr, "Update the access and modification times of each FILE to the current time.\n"
+			"Usage: touch [-mac] [<file/directory> ...]\n"
+			"\t -m: change only modification time"
+			"\t -a: change only access time"
+			"\t -c: do not create files"
+			"For more information, please consult man\n");
+}
+
+bool isValidPath(char *path) {
+	#if DEBUG
+		printf("chegou no isValidPath\n");
+	#endif
+
+	struct stat statBuffer;
+
+	if(lstat(path, &statBuffer) != -1)
+		return true;
+	else {
+		printf("ERROR %d: %s\n", errno, strerror(errno));
+		return false;
+	}
+}
+
+
+bool fileExists(char* fullpath) {
+	#if DEBUG
+		printf("chegou no fileExists\n");
+	#endif
+
+	struct stat statBuffer;
+
+	if(lstat(fullpath, &statBuffer) != -1)
+		return true;
+	else
+		return false;
+
+}
+
+void touch (char* path, const char* file, bool access, bool create, bool modification) {
+	// struct stat statBuffer;
+
+	char fullpath[1024] = "";
+	strcat(fullpath, path);
+	strcat(fullpath, "/");
+	strcat(fullpath, file);
+	time_t agora;
+	char hora[1024];
+	struct tm* time_info;
+	// int fd;
+
+	#if DEBUG
+	printf("%s\n",fullpath);
+	#endif
+
+	if (isValidPath(path))
+	{
+		#if DEBUG
+		printf("passou do isValidPath\n");
+		#endif
+
+		if(fileExists(fullpath)) {
+			//tenta abrir
+			#if VERBOSE
+			printf("Arquivo atualizado\n");
+			#endif
+
+			if(access && modification){
+				time(&agora);
+				time_info = localtime(&agora);
+				strftime(hora,1024,"%c",time_info);
+				printf("Mudamos o tempo para %s\n", hora);
+			}
+			if(access && !modification) {
+				time(&agora);
+				time_info = localtime(&agora);
+				strftime(hora,1024,"%c",time_info);
+				printf("Mudamos o tempo de acesso para %s\n", hora);
+			}
+			if(!access && modification) {
+				time(&agora);
+				time_info = localtime(&agora);
+				strftime(hora,1024,"%c",time_info);
+				printf("Mudamos o tempo de modificacao para %s\n", hora);
+			}
+
+			// muda os acessos
+		}
+		else {
+			if(create){
+				#if VERBOSE
+				printf("Nao existe e nao vou criar\n");
+				#endif
+				return;
+			}
+			else {
+			//cria vazia
+				#if VERBOSE
+				printf("Arquivo criado!\n");
+				#endif
+			}
+		}
+
+	}
+
+	return;
+
+}
+
+
+
+
+
+int main(int argc, char *argv[])
+{
+	char path[1024];
+	getcwd(path, sizeof(path));
+	bool create, access, modification = false;
+	int option;
+
+	//checa argumentos
+	if(argc == 1 || argc > 3) {
+		printUsage();
+		return -1;
+	}
+
+
+	while((option = getopt(argc, argv, "acm")) != -1) {
+		switch(option) {
+			case 'a':
+				access = true;
+				break;
+
+			case 'c':
+				create = true;
+				break;		
+
+			case 'm':
+				modification = true;
+				break;
+
+			default:
+				printUsage();
+				break;
+		}
+	}
+	//bota o alvo no final do path
+	if(argc == 2) {
+
+		touch(path, argv[1], access,create, modification);
+	}
+
+	else
+		touch(path, argv[2], access,create, modification);
+
+	return 0;
+
+}
